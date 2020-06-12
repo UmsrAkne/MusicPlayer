7c573421f4a4267fd50ef1d1f836cf453973734c
Akn <sibata486@gmail.com>
2020-06-12 21:33:12 +0900

    クラス追加 / 複数のSoundPlayerを保持するクラスを追加 #6

---
+++ MusicPlayer/model/DoubleSoundPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.model {
+    class DoubleSoundPlayer {
+        private List<SoundPlayer> players;
+
+        public DoubleSoundPlayer() {
+            players = new List<SoundPlayer>(2); // 今の所、要素数２より大きくする必要はない
+            players.Add(new SoundPlayer());
+            players.Add(new SoundPlayer());
+        }
+    }
+}
