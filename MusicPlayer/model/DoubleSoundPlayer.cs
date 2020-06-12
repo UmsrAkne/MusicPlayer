edbda53eb71df57d602cef2c04e7db0347362d98
Akn <sibata486@gmail.com>
2020-06-12 21:57:53 +0900

    機能追加 / SoundPlayerの入れ替え、取得機能を追加 #6

--- MusicPlayer/model/DoubleSoundPlayer.cs
+++ MusicPlayer/model/DoubleSoundPlayer.cs
@@ -7,11 +7,28 @@ using System.Threading.Tasks;
 namespace MusicPlayer.model {
     class DoubleSoundPlayer {
         private List<SoundPlayer> players;
+        private PlayerIndex currentPlayerIndex = PlayerIndex.First;
+
+        enum PlayerIndex :int {
+            First = 0,
+            Second = 1
+        }
 
         public DoubleSoundPlayer() {
             players = new List<SoundPlayer>(2); // 今の所、要素数２より大きくする必要はない
             players.Add(new SoundPlayer());
             players.Add(new SoundPlayer());
+        }
+
+        private void SwitchCurrentPlayerIndex() {
+            if (currentPlayerIndex == PlayerIndex.First) currentPlayerIndex = PlayerIndex.Second;
+            if (currentPlayerIndex == PlayerIndex.Second) currentPlayerIndex = PlayerIndex.First;
+        }
+
+        private SoundPlayer CurrentPlayer {
+            get {
+                return players[(int)currentPlayerIndex];
+            }
         }
     }
 }
