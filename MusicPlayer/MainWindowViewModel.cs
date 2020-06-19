17c024ddc1002134efe02d4369217a83727b0b22
Akn <sibata486@gmail.com>
2020-06-19 19:02:11 +0900

    機能追加 / ディレクトリツリーのソースとビューモデルのプロパティをバインド #14

--- MusicPlayer/MainWindowViewModel.cs
+++ MusicPlayer/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicPlayer.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,8 @@ using System.Threading.Tasks;
 
 namespace MusicPlayer {
     class MainWindowViewModel {
+
+        public List<MediaDirectory> Directory { get; set; }
 
     }
 }
---
+++ MusicPlayer/model/MediaDirectory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.model {
+    class MediaDirectory {
+
+        public String Name { get; set; }
+        public List<MediaDirectory> ChildDirectory { get; set; }
+
+    }
+}
