1f0f5808e610899d10bcf887658223e16273240e
Akn <sibata486@gmail.com>
2020-06-20 14:56:26 +0900

    仕様変更 / MediaDirectoryのNameの取得元を内部のFileInfoから取得に変更

--- MusicPlayer/model/MediaDirectory.cs
+++ MusicPlayer/model/MediaDirectory.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,8 +9,15 @@ using System.Threading.Tasks;
 namespace MusicPlayer.model {
     class MediaDirectory {
 
-        public String Name { get; set; }
+        public String Name {
+            get {
+                if (FileInfo == null) return "";
+                else return FileInfo.Name;
+            }
+        }
+
         public List<MediaDirectory> ChildDirectory { get; set; }
+        public FileInfo FileInfo { get; set; }
 
     }
 }
