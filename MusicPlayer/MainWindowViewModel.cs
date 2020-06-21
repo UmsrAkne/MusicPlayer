c395c3b14b0ea54996e7cbc681f7eb4bf29d336c
Akn <sibata486@gmail.com>
2020-06-21 15:39:08 +0900

    機能追加 / MainWindow.Directoryが変更通知を出すよう修正

--- MusicPlayer/MainWindowViewModel.cs
+++ MusicPlayer/MainWindowViewModel.cs
@@ -1,14 +1,26 @@
 ﻿using MusicPlayer.model;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MusicPlayer {
-    class MainWindowViewModel {
+    class MainWindowViewModel : BindableBase{
 
-        public List<MediaDirectory> Directory { get; set; }
+        private List<MediaDirectory> directory = new List<MediaDirectory>();
+
+        public List<MediaDirectory> Directory {
+            get {
+                return directory;
+            }
+            private set {
+                SetProperty(ref directory, value);
+                directory = value;
+            }
+        }
 
     }
 }
