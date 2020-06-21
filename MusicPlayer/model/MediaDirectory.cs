29dff3a371022a64cee14997071762a58c71a283
Akn <sibata486@gmail.com>
2020-06-21 15:31:51 +0900

    機能追加 / MediaDirectory に子ディレクトリを取得するメソッド、コマンドを追加

--- MusicPlayer/model/MediaDirectory.cs
+++ MusicPlayer/model/MediaDirectory.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Commands;
 
 namespace MusicPlayer.model {
     class MediaDirectory {
@@ -19,5 +20,23 @@ namespace MusicPlayer.model {
         public List<MediaDirectory> ChildDirectory { get; set; }
         public FileInfo FileInfo { get; set; }
 
+        public MediaDirectory() {
+            GetChildsCommand = new DelegateCommand(
+                () => { getChild(); },
+                () => { return true; }
+            );
+        }
+
+        private void getChild() {
+            ChildDirectory = new List<MediaDirectory>();
+            string[] childFileNames = System.IO.Directory.GetDirectories(FileInfo.FullName);
+            foreach (string n in childFileNames) {
+                var md = new MediaDirectory();
+                md.FileInfo = new FileInfo(n);
+                ChildDirectory.Add(md);
+            }
+        }
+
+        public DelegateCommand GetChildsCommand { get; private set; }
     }
 }
