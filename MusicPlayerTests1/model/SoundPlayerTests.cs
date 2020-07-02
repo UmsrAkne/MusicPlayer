203e91f0ae2d655181be07e6ad1826723a2afe09
Akn <sibata486@gmail.com>
2020-07-02 17:02:01 +0900

    テストプロジェクトを追加

---
+++ MusicPlayerTests1/model/SoundPlayerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MusicPlayer.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer.model.Tests {
+    [TestClass()]
+    public class SoundPlayerTests {
+        [TestMethod()]
+        public void playTest() {
+            Assert.Fail();
+        }
+    }
+}
