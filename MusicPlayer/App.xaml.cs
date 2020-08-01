352250725969aa1c8ab560f49a57c1a02c7e45d2
Akn <sibata486@gmail.com>
2020-08-01 19:35:30 +0900

    仕様変更 / ダイアログを出せるようAppクラスの内容を変更

--- MusicPlayer/App.xaml.cs
+++ MusicPlayer/App.xaml.cs
@@ -1,15 +1,26 @@
-﻿using System;
+﻿using Prism.Ioc;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Prism.Unity;
 
 namespace MusicPlayer {
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
-    public partial class App : Application {
+    public partial class App {
+
+        protected override Window CreateShell() {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+            // ビューとビューモデルを登録
+            containerRegistry.RegisterDialog<SettingWindow, SettingWindowViewModel>();
+        }
     }
 }
