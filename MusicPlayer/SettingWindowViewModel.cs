9b3da79d131ea37fe9bfb638bbc0f81fd3342f27
Akn <sibata486@gmail.com>
2020-08-01 19:33:38 +0900

    クラス追加 / 設定用ウィンドウのビューとビューモデルを追加

---
+++ MusicPlayer/SettingWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Services.Dialogs;
+using Prism.Commands;
+using System.IO;
+
+namespace MusicPlayer {
+    class SettingWindowViewModel : BindableBase, IDialogAware {
+        public string Title => "settingWindowTitle";
+
+        public event Action<IDialogResult> RequestClose;
+
+        private DialogParameters dialogParameters = new DialogParameters();
+
+        private DelegateCommand yesCommand;
+        public DelegateCommand YesCommand {
+            get => yesCommand ?? (yesCommand = new DelegateCommand(
+                () => {
+                    // yesボタンを押したときの実行内容
+                    //var ret = new DialogResult(ButtonResult.Yes, dialogParameters);
+                    //dialogParameters.Add("key1", "str");
+                    //this.RequestClose?.Invoke( ret );
+                }
+            ));
+        }
+
+        public DelegateCommand CloseDialogCommand { get; set; }
+
+        public SettingWindowViewModel() {
+            CloseDialogCommand = new DelegateCommand(() => RequestClose?.Invoke(new DialogResult()));
+
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters) {
+        }
+
+        public virtual bool CanCloseDialog() {
+            return true;
+        }
+
+        public virtual void OnDialogClosed() {
+
+        }
+
+    }
+}
