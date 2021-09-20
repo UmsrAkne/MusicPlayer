b3f7914897401c8ad93908f675828b8e4ffcc170
Akn <sibata486@gmail.com>
2021-09-20 23:21:23 +0900

    refactor / MainWindowViewModel を整形

--- MusicPlayer/viewModels/MainWindowViewModel.cs
+++ MusicPlayer/viewModels/MainWindowViewModel.cs
@@ -12,27 +12,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MusicPlayer {
-    class MainWindowViewModel : BindableBase{
+namespace MusicPlayer
+{
+    class MainWindowViewModel : BindableBase
+    {
 
         private DoubleSoundPlayer doubleSoundPlayer;
 
-        public DoubleSoundPlayer DoubleSoundPlayer {
+        public DoubleSoundPlayer DoubleSoundPlayer
+        {
             get { return doubleSoundPlayer; }
         }
 
-        public TreeViewModel TreeViewModel {
+        public TreeViewModel TreeViewModel
+        {
             get;
         }
 
         private List<IndexedFileInfo> mediaFiles = new List<IndexedFileInfo>();
-        public List<IndexedFileInfo> MediaFiles {
-            get {
+        public List<IndexedFileInfo> MediaFiles
+        {
+            get
+            {
                 return mediaFiles;
             }
-            private set {
+            private set
+            {
 
-                for(int i = 0; i < value.Count; i++) {
+                for (int i = 0; i < value.Count; i++)
+                {
                     value[i].Index = i + 1; // 表示番号は１始まりとしたいので +1
                 }
 
@@ -40,18 +48,22 @@ namespace MusicPlayer {
             }
         }
 
-        public int WindowWidth {
+        public int WindowWidth
+        {
             get => Properties.Settings.Default.WindowWidth;
-            set {
+            set
+            {
                 RaisePropertyChanged();
                 Properties.Settings.Default.WindowWidth = value;
                 Properties.Settings.Default.Save();
             }
         }
 
-        public int WindowHeight {
+        public int WindowHeight
+        {
             get => Properties.Settings.Default.WindowHeight;
-            set {
+            set
+            {
                 RaisePropertyChanged();
                 Properties.Settings.Default.WindowHeight = value;
                 Properties.Settings.Default.Save();
@@ -59,26 +71,32 @@ namespace MusicPlayer {
         }
 
         private DelegateCommand<Object> mediaFilesSettingCommand;
-        public DelegateCommand<Object> MediaFilesSettingCommand {
+        public DelegateCommand<Object> MediaFilesSettingCommand
+        {
             get => mediaFilesSettingCommand ?? (mediaFilesSettingCommand = new DelegateCommand<object>(
-                (object param) => {
+                (object param) =>
+                {
                     MediaDirectory info = (MediaDirectory)param;
                     MediaFiles = new List<IndexedFileInfo>();
                     List<IndexedFileInfo> mf = new List<IndexedFileInfo>();
 
-                    if (info.IsM3U) {
+                    if (info.IsM3U)
+                    {
                         var fileList = info.makeFileListFromM3U();
-                        foreach(FileInfo f in fileList) {
+                        foreach (FileInfo f in fileList)
+                        {
                             mf.Add(new IndexedFileInfo(f));
                         }
                     }
-                    else {
+                    else
+                    {
                         string[] fileNames = System.IO.Directory.GetFiles(info.FileInfo.FullName);
                         IEnumerable<String> selectedList = from name in fileNames
                                                            where name.EndsWith(".mp3")
                                                            select name;
 
-                        foreach(string n in selectedList) {
+                        foreach (string n in selectedList)
+                        {
                             mf.Add(new IndexedFileInfo(new FileInfo(n)));
                         }
                     }
@@ -96,18 +114,23 @@ namespace MusicPlayer {
         private PlayerSetting playerSetting;
 
         private DelegateCommand showSettingDialogCommand;
-        public DelegateCommand ShowSettingDialogCommand {
+        public DelegateCommand ShowSettingDialogCommand
+        {
             get => showSettingDialogCommand ?? (showSettingDialogCommand = new DelegateCommand(
-                () => {
+                () =>
+                {
                     var param = new DialogParameters();
                     param.Add(nameof(PlayerSetting), playerSetting);
                     dialogService.ShowDialog(nameof(SettingWindow), param,
-                        (IDialogResult result) => {
-                            if (result.Parameters.GetValue<PlayerSetting>(nameof(SettingWindowViewModel.Setting)) == null) {
+                        (IDialogResult result) =>
+                        {
+                            if (result.Parameters.GetValue<PlayerSetting>(nameof(SettingWindowViewModel.Setting)) == null)
+                            {
                                 // バツを押して閉じた時とかに値がNullになっている
                                 return;
                             }
-                            else {
+                            else
+                            {
                                 PlayerSetting pSettings = result.Parameters.GetValue<PlayerSetting>(nameof(SettingWindowViewModel.Setting));
                                 doubleSoundPlayer.SwitchingDuration = pSettings.SwitchingDuration;
                                 Properties.Settings.Default.SwitchinDuration = pSettings.SwitchingDuration;
@@ -125,9 +148,11 @@ namespace MusicPlayer {
             ));
         }
 
-        public DelegateCommand ShowLogWindowCommand {
+        public DelegateCommand ShowLogWindowCommand
+        {
             #region
-            get => showLogWindowCommand ?? (showLogWindowCommand = new DelegateCommand(() => {
+            get => showLogWindowCommand ?? (showLogWindowCommand = new DelegateCommand(() =>
+            {
                 var param = new DialogParameters();
                 dialogService.ShowDialog(nameof(HistoryWindow), param, (IDialogResult result) => { }
                 );
@@ -137,7 +162,8 @@ namespace MusicPlayer {
         #endregion
 
 
-        public MainWindowViewModel(IDialogService _dialogService) {
+        public MainWindowViewModel(IDialogService _dialogService)
+        {
             dialogService = _dialogService;
 
             string lastVisitedDirectoryPath = Properties.Settings.Default.lastVisitedDirectoryPath;
@@ -165,19 +191,22 @@ namespace MusicPlayer {
             DoubleSoundPlayer.Volume = Properties.Settings.Default.Volume;
 
             PlayCommand = new DelegateCommand(
-                () => {
+                () =>
+                {
                     doubleSoundPlayer.Files = MediaFiles;
                     doubleSoundPlayer.play();
                 },
                 () => { return MediaFiles != null && MediaFiles.Count > 0; }
-            ).ObservesProperty(() => MediaFiles );
+            ).ObservesProperty(() => MediaFiles);
 
         }
 
         private DelegateCommand randomSortCommand;
-        public DelegateCommand RandomSortCommand {
+        public DelegateCommand RandomSortCommand
+        {
             get => randomSortCommand ?? (randomSortCommand = new DelegateCommand(
-                () => {
+                () =>
+                {
                     Random r = new Random();
                     MediaFiles = MediaFiles.OrderBy(m => r.Next(MediaFiles.Count)).ToList();
                 },
@@ -186,10 +215,13 @@ namespace MusicPlayer {
         }
 
         private DelegateCommand nameOrderSortCommand;
-        public DelegateCommand NameOrderSortCommand {
+        public DelegateCommand NameOrderSortCommand
+        {
             get => nameOrderSortCommand ?? (nameOrderSortCommand = new DelegateCommand(
-                () => {
-                    if (MediaFiles != null && MediaFiles.Count > 0) {
+                () =>
+                {
+                    if (MediaFiles != null && MediaFiles.Count > 0)
+                    {
                         MediaFiles = MediaFiles.OrderBy(m => m.Name).ToList();
                     }
 
