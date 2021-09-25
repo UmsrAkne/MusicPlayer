3eb1df82dbe188c4561ba45de6fb91ac22df43a5
Akn <sibata486@gmail.com>
2021-09-25 22:57:04 +0900

    add / サウンドライブラリ、NAudio を使用するための変更

---
+++ MusicPlayer/Models/NAudioSound.cs
@@ -0,0 +1,49 @@
+﻿namespace MusicPlayer.Models
+{
+    using System;
+
+    public class NAudioSound : ISound
+    {
+        private string url;
+
+        public event EventHandler MediaEnded;
+
+        public event EventHandler MediaStarted;
+
+        public event EventHandler LoadCompleted;
+
+        public event EventHandler NearTheEnd;
+
+        public bool Playing => throw new NotImplementedException();
+
+        public bool Loading => throw new NotImplementedException();
+
+        public string URL { get => url; set => url = value; }
+
+        public int Volume { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public double Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public double Duration => throw new NotImplementedException();
+
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Resume()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
