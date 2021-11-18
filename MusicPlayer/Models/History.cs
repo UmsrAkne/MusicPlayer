ed5eafe9a8c73f44095a9c380eb4b77dc8322064
Akn <sibata486@gmail.com>
2021-11-18 14:31:31 +0900

    add / DB のテーブルを表すクラス、DbContext を継承したクラスを追加

---
+++ MusicPlayer/Models/History.cs
@@ -0,0 +1,26 @@
+﻿namespace MusicPlayer.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class History
+    {
+        [Key]
+        [Required]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("full_name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [Column("last_listen_date")]
+        public DateTime LastListenDate { get; set; }
+
+        [Required]
+        [Column("listen_count")]
+        public int ListenCount { get; set; }
+    }
+}
---
+++ MusicPlayer/Models/HistoryDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace MusicPlayer.Models
+{
+    using System.Data.SQLite;
+    using System.IO;
+    using Microsoft.Data.Sqlite;
+    using Microsoft.EntityFrameworkCore;
+
+    public class HistoryDbContext : DbContext
+    {
+        public DbSet<History> Histories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!File.Exists("History.sqlite"))
+            {
+                SQLiteConnection.CreateFile("History.sqlite");
+            }
+
+            var connectionString = new SqliteConnectionStringBuilder { DataSource = @"History.sqlite" }.ToString();
+            optionsBuilder.UseSqlite(new SqliteConnection(connectionString));
+        }
+    }
+}
