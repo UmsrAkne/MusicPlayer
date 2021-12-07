3613b5e76065b978a3c65b3c7636987571ad6e1f
Akn <sibata486@gmail.com>
2021-12-07 22:31:42 +0900

    add / VolumeController.Fader() を実装

--- MusicPlayer/Models/VolumeController.cs
+++ MusicPlayer/Models/VolumeController.cs
@@ -1,12 +1,17 @@
 ﻿namespace MusicPlayer.Models
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     public class VolumeController
     {
-        public int ExecuteCountPerSec { get; set; }
+        public int ExecuteCountPerSec { get; set; } = 4;
 
-        private List<ISound> PlayingSounds { get; } = new List<ISound>();
+        public int SwitchingDuration { get; set; } = 15;
+
+        public float MaxVolume { get; set; } = 1.0f;
+
+        private List<ISound> PlayingSounds { get; set; } = new List<ISound>();
 
         public void AddPlayingSound(ISound sound)
         {
@@ -15,6 +20,32 @@
 
         public void Fader()
         {
+            PlayingSounds = PlayingSounds.Where(sound => sound.Playing).ToList();
+
+            float changeAmount = MaxVolume / SwitchingDuration / ExecuteCountPerSec * 1.02f;
+
+            PlayingSounds.Where(sound => sound.Duration > SwitchingDuration * 2.5).ToList().ForEach(sound =>
+            {
+                // 曲の開始部分の音量調節処理
+                if (sound.Position < SwitchingDuration && sound.Volume <= MaxVolume)
+                {
+                    sound.Volume += changeAmount;
+                    if (sound.Volume >= MaxVolume)
+                    {
+                        sound.Volume = MaxVolume;
+                    }
+                }
+
+                // 曲の終了部分の音量調節処理
+                if (sound.Position > sound.Duration - SwitchingDuration && sound.Volume >= 0)
+                {
+                    sound.Volume -= changeAmount;
+                    if (sound.Volume <= 0)
+                    {
+                        sound.Volume = 0;
+                    }
+                }
+            });
         }
     }
 }
--- MusicPlayerTests3/Models/VolumeControllerTests.cs
+++ MusicPlayerTests3/Models/VolumeControllerTests.cs
@@ -1,6 +1,8 @@
 ﻿namespace MusicPlayer.Models.Tests
 {
+    using System.Collections.Generic;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MusicPlayerTests3.Models;
 
     [TestClass]
     public class VolumeControllerTests
@@ -8,7 +10,77 @@
         [TestMethod]
         public void FaderTest()
         {
-            Assert.Fail();
+            var volumeController = new VolumeController();
+            volumeController.MaxVolume = 1.0f;
+            volumeController.SwitchingDuration = 15;
+            volumeController.ExecuteCountPerSec = 4;
+
+            var dummySounds = new List<DummySound>()
+            {
+                new DummySound(),
+                new DummySound(),
+            };
+
+            dummySounds.ForEach(s =>
+            {
+                s.Duration = 40;
+                s.Volume = 0;
+            });
+
+            volumeController.AddPlayingSound(dummySounds[0]);
+            dummySounds[0].Play();
+
+            for (int i = 0; i < 4; i++)
+            {
+                dummySounds[0].Forward(0.25);
+                volumeController.Fader();
+            }
+
+            Assert.AreNotEqual(dummySounds[0].Volume, 0);
+
+            for (int i = 0; i < 56; i++)
+            {
+                dummySounds[0].Forward(0.25);
+                volumeController.Fader();
+            }
+
+            Assert.IsTrue(dummySounds[0].Volume >= 1.0, "再生開始から 15 秒地点で音量は 0 から max まで上昇");
+
+            for (int i = 0; i < 40; i++)
+            {
+                dummySounds[0].Forward(0.25);
+                volumeController.Fader();
+            }
+
+            System.Diagnostics.Debug.WriteLine("-----------------");
+
+            /// 更に 10秒経過で 25秒地点。次のサウンドを挿入する。
+
+            dummySounds[1].Volume = 0;
+            dummySounds[1].Play();
+            volumeController.AddPlayingSound(dummySounds[1]);
+
+            for (int i = 0; i < 30; i++)
+            {
+                dummySounds[0].Forward(0.25);
+                dummySounds[1].Forward(0.25);
+                volumeController.Fader();
+            }
+
+            Assert.IsTrue(dummySounds[0].Volume < 0.5, "音量 1 -> 0 に減少の途中");
+            Assert.AreNotEqual(dummySounds[0].Volume, 1);
+            Assert.IsTrue(dummySounds[1].Volume > 0.5, "音量 0 -> 1 に上昇の途中");
+            Assert.AreNotEqual(dummySounds[1].Volume, 0);
+
+            for (int i = 0; i < 30; i++)
+            {
+                dummySounds[0].Forward(0.25);
+                dummySounds[1].Forward(0.25);
+                volumeController.Fader();
+            }
+
+            Assert.AreEqual(dummySounds[0].Volume, 0);
+            Assert.AreEqual(dummySounds[1].Volume, 1);
         }
     }
 }
